1037ef25512f907023906c2a4d5e78d6702b2e84
Matias Banega <30711494+MBDIGITAL-INSUMOS@users.noreply.github.com>
2021-02-03 18:37:20 -0300

    Update run_storefront.sh
    update sh

--- run_storefront.sh
+++ run_storefront.sh
@@ -1,3 +1,4 @@
-export APP_MOUNT_URI=/dashboard/
-export API_URI=http://3.6.136.178:8000/graphql/
-cd /home/ubuntu/codes/backend/storefront && npm start
+export API_URI=http://localhost:8000/graphql/ 
+export APP_MOUNT_URI="/storefront" 
+export STATIC_URL="/storefront"
+cd /home/ubuntu/construirte/construirte-storefront && npm start
